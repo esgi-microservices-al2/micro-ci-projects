211cf6547d24a7710bfd97861c37065cdb546b02
DELMER <d.pierrebox@gmail.com>
2020-07-14 20:02:03 +0200

    Fixing issues

--- src/main/java/com/example/projects/component/Receiver.java
+++ src/main/java/com/example/projects/component/Receiver.java
@@ -39,7 +39,7 @@ public class Receiver {
             JSONObject input = (JSONObject) parser.parse(message);
             JSONObject repo = (JSONObject) input.get("repository");
             JSONObject user = (JSONObject) input.get("user");
-            Project exists = projectRepository.findByUrl(input.get("clone_url").toString());
+            Project exists = projectRepository.findByUrl(repo.get("clone_url").toString());
             if(exists != null && exists.getUrl() != null){
                 ResponseEntity<String>  response = buildResource.generateBuild(exists);
                 if(response.getStatusCodeValue() == 200){
@@ -53,7 +53,7 @@ public class Receiver {
                 created.setOwner(user.get("username").toString());
                 created.setUrl(input.get("clone_url").toString());
                 created.setName(input.get("name").toString());
-                created.setDefaultBranch(input.get("default_branch").toString());
+                created.setDefaultBranch(repo.get("default_branch").toString());
                 projectRepository.save(created);
                 ResponseEntity<String>  response = buildResource.generateBuild(created);
                 if(response.getStatusCodeValue() == 200){
