9521ce0ddc0ce6caa5c7e1ecbffc80f07bcf24e2
DELMER <d.pierrebox@gmail.com>
2020-07-15 02:14:06 +0200

    Fixing consuming message

--- src/main/java/com/example/projects/component/Receiver.java
+++ src/main/java/com/example/projects/component/Receiver.java
@@ -6,16 +6,20 @@ import com.example.projects.repositories.BuildRepository;
 import com.example.projects.repositories.ProjectRepository;
 import com.example.projects.resources.BuildResource;
 import com.example.projects.services.Tools;
+import com.rabbitmq.client.Channel;
 import org.apache.coyote.Response;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Optional;
@@ -32,38 +36,43 @@ public class Receiver {
     private CountDownLatch latch = new CountDownLatch(1);
 
     @RabbitListener(queues = "al2.payload.q")
-    public void receiveMessage(String message) {
+    public void receiveMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         try{
             Tools.log(1, "Receiving a new message");
             JSONParser parser = new JSONParser();
             JSONObject input = (JSONObject) parser.parse(message);
+            channel.basicAck(tag, false);
             JSONObject repo = (JSONObject) input.get("repository");
             JSONObject user = (JSONObject) input.get("user");
-            Project exists = projectRepository.findByUrl(repo.get("clone_url").toString());
-            if(exists != null && exists.getUrl() != null){
-                ResponseEntity<String>  response = buildResource.generateBuild(exists);
-                if(response.getStatusCodeValue() == 200){
-                    Tools.log(1, "Build generated for project : "+exists.get_id());
+            if(!repo.get("clone_url").toString().equals("") && repo.get("clone_url") != null){
+                Project exists = projectRepository.findByUrl(repo.get("clone_url").toString());
+                if(exists != null && exists.getUrl() != null){
+                    ResponseEntity<String>  response = buildResource.generateBuild(exists);
+                    if(response.getStatusCodeValue() == 200){
+                        Tools.log(1, "Build generated for project : "+exists.get_id());
+                    }else{
+                        Tools.log(1, "Build creation failed for project : "+exists.get_id());
+                    }
                 }else{
-                    Tools.log(1, "Build creation failed for project : "+exists.get_id());
-                }
-            }else{
-                Project created = new Project();
-                created.set_public(true);
-                created.setOwner(user.get("username").toString());
-                created.setUrl(repo.get("clone_url").toString());
-                created.setName(repo.get("name").toString());
-                created.setDefaultBranch(repo.get("default_branch").toString());
-                projectRepository.save(created);
-                ResponseEntity<String>  response = buildResource.generateBuild(created);
-                if(response.getStatusCodeValue() == 200){
-                    Tools.log(1, "Build generated for project : "+created.get_id());
-                }else{
-                    Tools.log(2, "Build creation failed for project : "+created.get_id());
+                    Project created = new Project();
+                    created.set_public(true);
+                    created.setOwner(user.get("username").toString());
+                    created.setUrl(repo.get("clone_url").toString());
+                    created.setName(repo.get("name").toString());
+                    created.setDefaultBranch(repo.get("default_branch").toString());
+                    projectRepository.save(created);
+                    ResponseEntity<String>  response = buildResource.generateBuild(created);
+                    if(response.getStatusCodeValue() == 200){
+                        Tools.log(1, "Build generated for project : "+created.get_id());
+                    }else{
+                        Tools.log(2, "Build creation failed for project : "+created.get_id());
+                    }
                 }
             }
         } catch (ParseException e) {
             e.printStackTrace();
+            channel.basicAck(tag, false);
+            latch.countDown();
             System.exit(0);
         }
         latch.countDown();
